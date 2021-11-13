769cdafd2c8c1790cc3cd05c00a569c9cd494540
arafathossion <arafathossionrahman@gmail.com>
2021-11-13 13:23:25 +0600

    almost done without home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,92 @@ function openCity(evt, cityName) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
+
+
+
+
+  filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("actives");
+    current[0].className = current[0].className.replace(" actives", "");
+    this.className += " actives";
+  });
+}
+
+
+
+
+$('.test-popup-link').magnificPopup({
+  type: 'image',
+  // other options
+  gallery:{
+    enabled:true
+  }
+});
+
+
+
+
+
+//  Barfiller JS
+$("#barfiller").mouseenter(function () { 
+  $('#bar1').barfiller({
+    duration: 1100,
+});
+  $('#bar2').barfiller({
+    duration: 1300,
+});
+  $('#bar3').barfiller({
+    duration: 1500,
+});
+  $('#bar4').barfiller({
+    duration: 1700,
+});
+  $('#bar5').barfiller({
+    duration: 1900,
+});
+  $('#bar6').barfiller({
+    duration: 2100,
+});
+}); 
